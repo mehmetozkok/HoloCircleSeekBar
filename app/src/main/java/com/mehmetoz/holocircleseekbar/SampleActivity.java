78b394e8b002ed7bed3338568f3bf906a3b11f03
Mehmet <mehmetozkok83@yahoo.com>
2023-11-20 23:57:43 +0300

    ok

---
+++ app/src/main/java/com/mehmetoz/holocircleseekbar/SampleActivity.java
@@ -0,0 +1,16 @@
+package com.mehmetoz.holocircleseekbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class SampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+
+}
